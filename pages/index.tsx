53e2020cd2d16262b47b2fceec9946320fd8e364
abrabant <abrabant@student.42.fr>
2022-02-16 15:26:32 +0100

    Add title and meta description tags for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,8 +20,8 @@ const Home: NextPageWithLayout = () => {
   return (
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Head>
-          <title>Feature</title>
-          <link rel="icon" href="/favicon.ico" />
+          <title>{t('title')}</title>
+          <meta name="description" content={t('description')} />
         </Head>
 
         <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
