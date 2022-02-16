4152502edb85a7177ca18d0a2a8915e486087dbc
abrabant <abrabant@student.42.fr>
2022-02-16 10:14:04 +0100

    Add NextJS i18n configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  /**
+   * If prefix for the default locale is wanted:
+   * https://nextjs.org/docs/advanced-features/i18n-routing#prefixing-the-default-locale
+   */
+  i18n: {
+    locales: [ 'en-US', 'fr' ],
+    defaultLocale: 'en-US'
+  }
+
 }
