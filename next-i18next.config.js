1a89991dabacb5380027356c415612336ac22951
abrabant <abrabant@student.42.fr>
2022-02-16 11:54:25 +0100

    Implement basic i18n using next-i18next, with basic translations

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  /* in case default locale prefix is wanted:
+  * https://nextjs.org/docs/advanced-features/i18n-routing#prefixing-the-default-locale */
+  i18n: {
+    locales: ['en-US', 'fr'],
+    defaultLocale: 'en-US',
+  },
+}
