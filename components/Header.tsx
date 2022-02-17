825002579ec34b5bba155b0cd45d182b7c10efce
abrabant <abrabant@student.42.fr>
2022-02-17 09:01:45 +0100

    Add hero section to the homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -92,7 +92,7 @@ const Header: React.FC = () => {
   const { t: translate } = useTranslation('header')
 
   return (
-    <Popover className="relative bg-white">
+    <Popover className="fixed top-0 left-0 right-0 z-40 bg-transparent">
       <div className="flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
         <div>
           <a href="#" className="flex">
@@ -114,7 +114,7 @@ const Header: React.FC = () => {
         <div className="hidden md:flex md:flex-1 md:items-center md:justify-between">
           <Popover.Group as="nav" className="flex space-x-10">
             <Link href="/product">
-              <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a className="text-base font-medium text-neutral-200 hover:text-white">
                 {translate('navitem_product')}
               </a>
             </Link>
@@ -123,15 +123,15 @@ const Header: React.FC = () => {
                 <>
                   <Popover.Button
                     className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                      open ? 'text-white' : 'text-neutral-200',
+                      'group inline-flex items-center rounded-md bg-transparent text-base font-medium hover:text-white focus:outline-none '
                     )}
                   >
                     {translate('navitem_features')}
                     <ChevronDownIcon
                       className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                        open ? 'text-neutral-200' : 'text-neutral-100',
+                        'ml-2 h-5 w-5 group-hover:text-white'
                       )}
                       aria-hidden="true"
                     />
@@ -198,62 +198,15 @@ const Header: React.FC = () => {
               )}
             </Popover>
 
-            <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                    )}
-                  >
-                    <span>{translate('navitem_blog')}</span>
-                    <ChevronDownIcon
-                      className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
-                      )}
-                      aria-hidden="true"
-                    />
-                  </Popover.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel className="absolute z-10 w-screen max-w-xs px-2 mt-3 left-1/2 -translate-x-1/2 transform sm:px-0">
-                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                        <div className="relative px-5 py-6 bg-white grid gap-6 sm:gap-8 sm:p-8">
-                          {resources.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="block p-3 -m-3 rounded-md hover:bg-gray-50"
-                            >
-                              <p className="text-base font-medium text-gray-900">
-                                {item.name}
-                              </p>
-                              <p className="mt-1 text-sm text-gray-500">
-                                {item.description}
-                              </p>
-                            </a>
-                          ))}
-                        </div>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
+            <Link href="/blog">
+              <a className="text-base font-medium text-neutral-200 hover:text-white">
+                {translate('navitem_blog')}
+              </a>
+            </Link>
           </Popover.Group>
           <div className="flex items-center md:ml-12">
             <Link href="/dashboard">
-              <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a className="text-base font-medium text-neutral-200 hover:text-white">
                 {translate('navitem_dashboard')}
               </a>
             </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 
 import { NextPageWithLayout } from './_app'
 import withMainLayout from '../layouts/withMainLayout'
+import { ChevronRightIcon } from '@heroicons/react/outline'
+import { useRef } from 'react'
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
@@ -18,21 +21,98 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
   }
 }
 
+const HeroSection: React.FC = () => {
+  const { t: translate } = useTranslation('homepage')
+
+  const bookDemoEmailInputField = useRef<HTMLInputElement | null>(null)
+
+  const handleDemoBookingSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    // using HTML5 validation for now
+    if ((e.target as any).reportValidity()) {
+      const email = bookDemoEmailInputField.current?.value
+      console.log(email)
+    } else {
+      console.log('Not a valid email')
+    }
+  }
+
+  return (
+    <section className="relative min-h-[70vh] overflow-hidden bg-gray-900 pb-4">
+      <div className="absolute -right-14 -top-14 flex justify-center">
+        <Image src="/logos/github.svg" height={850} width={850} />
+      </div>
+
+      <div className="relative z-10 flex min-h-[70vh] items-center pt-32 lg:px-4 lg:pt-8">
+        <div className="w-full lg:grid lg:grid-cols-5 lg:gap-8 lg:px-12">
+          <div className="px-2 sm:px-6 sm:text-center lg:col-span-3 lg:flex lg:items-center lg:px-0 lg:text-left">
+            <div className="lg:py-2 lg:pl-16">
+              <h1 className="mt-4 flex flex-col gap-y-2 text-center text-4xl font-extrabold tracking-tight text-white sm:mt-5 sm:text-5xl lg:mt-6 lg:text-left xl:text-6xl">
+                <span className="block">{translate('hero_heading_part1')}</span>
+                <span className="block text-indigo-400">
+                  {translate('hero_heading_part2')}
+                </span>
+              </h1>
+              <p className="mt-3 text-center text-base text-gray-300 sm:mt-5 sm:text-xl lg:max-w-3xl lg:text-left lg:text-lg xl:text-xl">
+                {translate('hero_subheading')}
+              </p>
+              <div className="mt-10 sm:mt-12">
+                <form
+                  className="sm:mx-auto sm:max-w-xl lg:mx-0"
+                  onSubmit={handleDemoBookingSubmit}
+                >
+                  <div className="sm:flex">
+                    <div className="min-w-0 flex-1">
+                      <label htmlFor="email" className="sr-only">
+                        Email address
+                      </label>
+                      <input
+                        required
+                        autoComplete="off"
+                        ref={bookDemoEmailInputField}
+                        id="email"
+                        type="email"
+                        placeholder="Enter your email"
+                        className="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-offset-2 focus:ring-offset-gray-900"
+                      />
+                    </div>
+                    <div className="mt-3 sm:mt-0 sm:ml-3">
+                      <button
+                        type="submit"
+                        className="block w-full rounded-md bg-indigo-500 px-4 py-3 font-medium text-white shadow hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-offset-2 focus:ring-offset-gray-900"
+                      >
+                        Book a demo
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center py-8 lg:col-span-2 lg:py-0">
+            <Image src="/hero_snippet.svg" width={579} height={377} />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
 const Home: NextPageWithLayout = () => {
   const { t: translate } = useTranslation('homepage')
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <>
       <Head>
         <title>{translate('title')}</title>
         <meta name="description" content={translate('description')} />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1>{translate('hero_heading')}</h1>
-        <h2>{translate('hero_subheading')}</h2>
+      <main>
+        <HeroSection />
       </main>
-    </div>
+    </>
   )
 }
 
