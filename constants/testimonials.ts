63efcb50b42e60514bf6ba95f632aebdbf59f948
abrabant <abrabant@student.42.fr>
2022-02-18 11:01:38 +0100

    Fix prettier format script glob and run formatting pass

--- constants/testimonials.ts
+++ constants/testimonials.ts
@@ -1,15 +1,15 @@
 export type Testimonial = {
-	i18nContent: string
-	author: string
-	role: string // i.e 'CEO', Lead developer', etc...
-	workplace: string
+  i18nContent: string
+  author: string
+  role: string // i.e 'CEO', Lead developer', etc...
+  workplace: string
 }
 
 export const testimonials: Testimonial[] = [
-	{
-		i18nContent: 'testimonial_example_content',
-		role: 'CEO',
-		workplace: 'Pure Insights',
-		author: 'Judith Black',
-	},
+  {
+    i18nContent: 'testimonial_example_content',
+    role: 'CEO',
+    workplace: 'Pure Insights',
+    author: 'Judith Black',
+  },
 ]
