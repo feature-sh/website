fde32e0f2e48cb30adeef1305823d19164e12f5a
abrabant <abrabant@student.42.fr>
2022-02-18 12:25:37 +0100

    Add random testimonial generation for testing

--- constants/testimonials.ts
+++ constants/testimonials.ts
@@ -1,8 +1,11 @@
+import { faker } from '@faker-js/faker';
+
 export type Testimonial = {
 	i18nContent: string
 	author: string
 	role: string // i.e 'CEO', Lead developer', etc...
 	workplace: string
+	photo: string
 }
 
 export const testimonials: Testimonial[] = [
@@ -11,5 +14,16 @@ export const testimonials: Testimonial[] = [
 		role: 'CEO',
 		workplace: 'Pure Insights',
 		author: 'Judith Black',
+		photo: 'https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80'
 	},
 ]
+
+export const genFakeTestimonials = (n: number = 5) => {
+	return new Array(n).fill(null).map<Testimonial>(() => ({
+		 author: `${faker.name.firstName()} ${faker.name.lastName()}`,
+		 workplace: faker.company.companyName(),
+		 role: faker.name.title(),
+		 i18nContent: 'testimonial_example_content', // this one will never change
+		 photo: faker.image.business()
+	}))
+}
