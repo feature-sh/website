e1504c67a7399ca6192ac79ef55a50eb165b7ad6
abrabant <abrabant@student.42.fr>
2022-03-07 10:29:32 +0100

    Replace HTML select by TailwindUI list box

--- components/language-switcher.tsx
+++ components/language-switcher.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 
 import { LanguageDict } from '../constants/lang'
+import { classNames } from '../utils/classNames'
 
 type LanguageSwitcherProps = {
   languages: LanguageDict
@@ -12,11 +15,9 @@ const LanguageSwitcher: React.FC<LanguageSwitcherProps> = ({ languages }) => {
   const router = useRouter()
   const { t: translate } = useTranslation('footer')
 
-  const handleLanguageSwitch = async (
-    ev: React.ChangeEvent<HTMLSelectElement>
-  ) => {
+  const handleLanguageSwitch = async (toLocale: string) => {
     await router.push(router.asPath, router.asPath, {
-      locale: ev.target.value,
+      locale: toLocale,
       scroll: false,
     })
   }
@@ -37,17 +38,86 @@ const LanguageSwitcher: React.FC<LanguageSwitcherProps> = ({ languages }) => {
           d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"
         />
       </svg>
-      <select
-        onChange={handleLanguageSwitch}
-        value={router.locale}
-        className={'px-3 py-1 outline-0'}
-      >
-        {Object.keys(languages).map((locale) => (
-          <option value={locale} key={locale}>
-            {translate(languages[locale].i18nDisplayName)}
-          </option>
-        ))}
-      </select>
+
+      <Listbox value={router.locale} onChange={handleLanguageSwitch}>
+        {({ open }) => (
+          <>
+            <Listbox.Label className="block text-sm font-medium text-gray-700">
+              Assigned to
+            </Listbox.Label>
+            <div className="relative mt-1">
+              <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+                <span className="block truncate">
+                  {translate(
+                    languages[router.locale as string].i18nDisplayName
+                  )}
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  <SelectorIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {Object.keys(languages).map((locale) => {
+                    const language = languages[locale]
+                    return (
+                      <Listbox.Option
+                        key={locale}
+                        className={({ active }) =>
+                          classNames(
+                            active
+                              ? 'bg-indigo-600 text-white'
+                              : 'text-gray-900',
+                            'relative cursor-default select-none py-2 pl-8 pr-4'
+                          )
+                        }
+                        value={locale}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <span
+                              className={classNames(
+                                selected ? 'font-semibold' : 'font-normal',
+                                'block truncate'
+                              )}
+                            >
+                              {translate(language.i18nDisplayName)}
+                            </span>
+
+                            {selected ? (
+                              <span
+                                className={classNames(
+                                  active ? 'text-white' : 'text-indigo-600',
+                                  'absolute inset-y-0 left-0 flex items-center pl-1.5'
+                                )}
+                              >
+                                <CheckIcon
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    )
+                  })}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
     </div>
   )
 }
