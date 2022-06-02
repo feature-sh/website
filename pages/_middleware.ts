b5ea6de838f42217d9d108f6d77906e76cf391e0
aurelien-brabant <main@aurelienbrabant.fr>
2022-06-02 10:19:21 +0200

    readd middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { GOOGLE_OPTIMIZE_COOKIE_NAME } from '../constants/google-optimize'
+import {
+  getGoogleOptimizeCurrentExperiment,
+  GoogleOptimizeExperimentVariant,
+} from '../lib/google-optimize'
+
+const optimizedPaths =
+  process.env.GOOGLE_OPTIMIZE_OPTIMIZED_PATHS?.split(' ') || []
+
+console.log({ optimizedPaths })
+
+export const middleware = (req: NextRequest) => {
+  let res = NextResponse.next()
+  let cookie = req.cookies[GOOGLE_OPTIMIZE_COOKIE_NAME]
+
+  if (!cookie) {
+    let n = Math.random() * 100
+    const experiment = getGoogleOptimizeCurrentExperiment()
+
+    if (!experiment) {
+      return res
+    }
+    const variant = experiment.variants.find((variant) => {
+      if (variant.weight >= n) return true
+      n -= variant.weight
+    }) as GoogleOptimizeExperimentVariant
+
+    cookie = `${experiment.id}.${variant.id}`
+  }
+
+  const [, variantId] = cookie.split('.')
+  const url = req.nextUrl.clone()
+
+  console.log({ path: url.pathname })
+
+  if (optimizedPaths.includes(url.pathname)) {
+	console.log({ cookie })
+
+    if (variantId !== '0') {
+      url.pathname = url.pathname.replace('/', `/${cookie}/`)
+    }
+    res = NextResponse.rewrite(url)
+  }
+
+  if (!req.cookies[GOOGLE_OPTIMIZE_COOKIE_NAME]) {
+    res.cookie(GOOGLE_OPTIMIZE_COOKIE_NAME, cookie)
+  }
+
+  return res
+}
